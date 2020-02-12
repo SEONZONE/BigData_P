93817fc2d81c9a1aa0c0c89fe6b7bd6630c5b79a
SEONZONE <48579953+SEONZONE@users.noreply.github.com>
2020-02-12 23:39:55 +0900

    인구피라미드 (1960~2050)
    인구피라미드 분석 gganimate 사용

--- korea.R
+++ korea.R
@@ -1,27 +1,46 @@
-install.packages("idbr")
-install.packages("tidyverse")
-install.packages("extrafont")
-install.packages("animation")
-install.packages("ggthemes")
-devtools::install_github('walkerke/idbr')
-library(countrycode)
-library(tidyverse)
-library(idbr) 
-library(ggplot2)
-library(animation)
+install.packages('gganimate')
+install.packages('gifski')
+install.packages('png')
+library(gganimate)
+library(gifski)
+library(png)
 library(dplyr)
-library(ggthemes)
-library(extrafont)
-loadfonts()
-countrycode('Korea', 'country.name', 'fips104')
-idb_api_key("e850bb017ada9a0f1f49f3d87832f9b6a30ab937")
+library(tidyr)
+library(ggplot2)
+library(reshape2)
+library(readxl)
+# ============== excel 파일 넣기 ==============
+datPop <- data.table::fread('material/population.csv', encoding='UTF-8')
+datPop2 <- read_excel('material/2021~2023.xls')
+datPop3 <- read_excel('material/2050.xlsx')
 
-male <- idb1('KS', 1990:2050, sex = 'male') %>%mutate(SEX = 'Male')
+head(datPop2)
+str(datPop3)
+View(datPop3)
+# ======================== 합치고 rbind() ======================== 
+datPop2Ani <- datPop2 %>% mutate(year=as.integer(year)) %>% gather(key='gender', value='population', male, female)
+datPopAni <- datPop %>% mutate(year=as.integer(year)) %>% gather(key='gender', value='population', male, female)
+datPop3Ani <- datPop3 %>% mutate(year=as.integer(year)) %>% gather(key='gender', value='population', male, female)
 
-female <- idb1('KS', 1990:2050, sex = 'female') %>%mutate(POP = POP * -1,SEX = 'Female')
+datPop4Ani <- rbind(datPop4Ani,datPop3Ani)
+View(datPop4Ani)
 
-korea <- bind_rows(male, female) %>%
-  mutate(abs_pop = abs(POP))
+datPopAni <- subset(datPopAni,select=-age)
+#================ gganimate 실행 =========================
 
-korea <- korea %>% mutate(SEX = ifelse(SEX == "Male", "남자", "여자"))
-
+ggplot(datPopAni, aes(x=age2, 
+                      y=ifelse(gender=='male', 
+                               population/10000, -population/10000), fill=gender)) +
+  geom_bar(stat='identity') +
+  geom_bar(stat='identity', width=0.9,alpha=0.8) +
+  scale_x_continuous(name='나이') +
+  scale_y_continuous(name='인구(만명)', labels=c(50, 25, 0, 25, 50)) + 
+  coord_flip() +
+  labs(title = '한국의 인구 구조 변화 ({frame_time} 년)')+
+  scale_fill_manual(name='성별', labels=c('여', '남'), values=c('darkorange', 'dodgerblue2'))+
+  theme_minimal() +
+  theme(legend.position='bottom', title=element_text(size=16)) + 
+  theme(legend.position='bottom') +
+  transition_time(year) + 
+  enter_fade() -> p
+p
