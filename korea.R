071b82169c61bc1f0b6e8dd5b1525bbe30416b4f
seonzone <tjdwhd2247@gmail.com>
2020-02-13 17:10:37 +0900

    노인재가서비스 현황 조사
    노인재가서비스 현황 조사

--- korea.R
+++ korea.R
@@ -18,17 +18,20 @@ head(datPop2)
 str(datPop3)
 View(datPop3)
 # ======================== 합치고 rbind() ======================== 
-datPop2Ani <- datPop2 %>% mutate(year=as.integer(year)) %>% gather(key='gender', value='population', male, female)
 datPopAni <- datPop %>% mutate(year=as.integer(year)) %>% gather(key='gender', value='population', male, female)
+datPop2Ani <- datPop2 %>% mutate(year=as.integer(year)) %>% gather(key='gender', value='population', male, female)
 datPop3Ani <- datPop3 %>% mutate(year=as.integer(year)) %>% gather(key='gender', value='population', male, female)
 
-datPop4Ani <- rbind(datPop4Ani,datPop3Ani)
-View(datPop4Ani)
-
 datPopAni <- subset(datPopAni,select=-age)
+
+datPop4Ani <- rbind(datPopAni,datPop2Ani)
+datPop5Ani <- rbind(datPop4Ani,datPop3Ani)
+View(datPop5Ani)
+
+
 #================ gganimate 실행 =========================
 
-ggplot(datPopAni, aes(x=age2, 
+ggplot(datPop5Ani, aes(x=age2, 
                       y=ifelse(gender=='male', 
                                population/10000, -population/10000), fill=gender)) +
   geom_bar(stat='identity') +
@@ -36,11 +39,13 @@ ggplot(datPopAni, aes(x=age2,
   scale_x_continuous(name='나이') +
   scale_y_continuous(name='인구(만명)', labels=c(50, 25, 0, 25, 50)) + 
   coord_flip() +
-  labs(title = '한국의 인구 구조 변화 ({frame_time} 년)')+
-  scale_fill_manual(name='성별', labels=c('여', '남'), values=c('darkorange', 'dodgerblue2'))+
+  labs(title = '한국의 인구 변화 ({frame_time} 년)')+
+  scale_fill_manual(name='성별', labels=c('여', '남'), values=c('#FF33CC', 'dodgerblue2'))+
   theme_minimal() +
   theme(legend.position='bottom', title=element_text(size=16)) + 
   theme(legend.position='bottom') +
   transition_time(year) + 
   enter_fade() -> p
 p
+
+anim_save("populationratio.gif")
